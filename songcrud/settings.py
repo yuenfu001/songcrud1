7496bd722422945aa38ed4d8775465a2676d01f6
yuenfu001 <liuyuenfu@gmail.com>
2022-11-02 22:47:27 +0100

    key

--- songcrud/settings.py
+++ songcrud/settings.py
@@ -21,10 +21,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = config("how")
+SECRET_KEY = "django-insecure-zm-gk)x41pbwpzsu49ixq5f9ipio)9=m+())bv+bi#$a@0b+y2"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config("hmm")
+DEBUG = "False"
 
 ALLOWED_HOSTS = []
 
